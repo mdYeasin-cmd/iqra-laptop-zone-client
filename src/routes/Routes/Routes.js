288453e3a945fa19a2c7f8dd2e5ec83dbbfedd7c
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-24 21:37:15 +0600

    Firebase config file added to the project

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../layouts/DashboardLayout";
 import Main from "../../layouts/Main";
+import Blog from "../../Pages/Blog/Blog";
 import Home from "../../Pages/Home/Home/Home";
 
 export const routes = createBrowserRouter([
@@ -10,7 +12,15 @@ export const routes = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
             }
         ]
+    },
+    {
+        path: '/dashboard',
+        element: <DashboardLayout></DashboardLayout>
     }
 ]);
