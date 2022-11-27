4c04e919386acfffdb92b368a90ac9916c1ffa5d
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-27 18:56:24 +0600

    All buyers and all sellers page design part added

--- src/Pages/Dashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,68 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllBuyers = () => {
+
+    const { data: buyers, isLoading } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/buyers`);
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
+
     return (
         <div>
-            this is all buyers page.
+            <h2 className="text-4xl text-center mt-3 font-semibold">All Sellers</h2>
+            <p>{buyers.length}</p>
+            <div className="overflow-x-auto">
+                <table className="table mt-4 mx-auto shadow-lg">
+
+                    <thead>
+                        <tr className="text-center">
+                            <th></th>
+                            <th>Seller Name</th>
+                            <th>Seller Email</th>
+                            <th>Verify Action</th>
+                            <th>Delete Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        {
+                            buyers.map((buyer, idx) => <tr key={buyer._id}>
+                                <th>{idx + 1}</th>
+                                <td>{buyer.name}</td>
+                                <td>{buyer.email}</td>
+                                <td>
+                                    <button
+                                        className="btn bg-red-700 hover:bg-red-600 border-0"
+                                    // onClick={() => handleAdvertise(product)}
+                                    >
+                                        Verify
+                                    </button>
+                                </td>
+                                <td>
+
+                                    <button
+                                        className="btn bg-red-700 hover:bg-red-600 border-0 ml-3"
+                                    // onClick={() => handleDelete(product._id)}
+                                    >Delete</button>
+                                </td>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
