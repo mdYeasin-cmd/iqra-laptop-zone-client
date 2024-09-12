e31f37e0e59ff37f4a9a56c5e2cb7c59bb6eda6a
mdYeasin-cmd <yeasin200011@gmail.com>
2024-09-12 19:08:52 +0600

    Add login and signup page ui

--- README.md
+++ README.md
@@ -1,36 +1,24 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Project Name & Live Site Link
 
-## Getting Started
+Name - Iqra Laptop Zone
+<br/>
+Live Site Link - https://iqra-laptop-zone.web.app/
 
-First, run the development server:
+<br/>
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+# Features
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+<li> Integrated Authentication System </li>
+<li> Sign in with Email/Password, Google </li>
+<li> Use MongoDB as Database system </li>
+<li> Admin and seller dashboard </li>
+<li> Nested route </li>
+<li> Private route </li>
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+<br/>
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+# Technologies
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<li> Next.js </li>
+<li> Tailwind CSS </li>
+<li> Firebase </li>
---
+++ src/app/signup/page.js
@@ -0,0 +1,86 @@
+import Link from "next/link";
+
+const SignupPage = () => {
+    return (
+        <div className="w-2/5 mx-auto shadow-2xl p-8 my-10 rounded-lg">
+            <h2 className="text-3xl w-4/12 mx-auto py-2 rounded-lg font-semibold text-center bg-red-200">Sign Up</h2>
+            <form>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        className="input input-bordered w-full"
+                    />
+                </div>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Phot URL</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="photoURL"
+                        className="input input-bordered w-full"
+                    />
+                </div>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        className="input input-bordered w-full"
+                    />
+                </div>
+
+                <div className="form-control w-full">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input
+                        type="password"
+                        name="password"
+                        className="input input-bordered w-full"
+                    />
+                </div>
+
+                <div className="form-control text-center mt-4">
+
+                    <p>Choose your account type?</p>
+
+                    <div className="flex items-center justify-center">
+
+                        <input className="ml-2" type="radio" id="buyer" name="userRole" value="buyer" defaultChecked />
+                        <label className="mr-3 ml-1" htmlFor="buyer">Buyer</label>
+                        <br />
+                        <input className="mr-1" type="radio" id="seller" name="userRole" value="seller" />
+                        <label htmlFor="seller">Seller</label>
+
+                    </div>
+
+                </div>
+
+                <div className="form-control w-full">
+                    <input
+                        type="submit"
+                        value="Sign Up"
+                        className="btn bg-red-700 hover:bg-red-600 border-0 mt-4 w-full"
+                    />
+                </div>
+            </form>
+            <div>
+                <p className="text-center mt-3 text-lg">
+                    <small>Already have an account? <Link className="text-blue-900" href="/login">Log In</Link></small>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default SignupPage;
